2ef8619469c1ce3794f1b9da04167ae951ec1a6f
Nicolas Von Dolinger <nicolasvondolinger@gmail.com>
2025-06-10 19:54:54 -0300

    Commit 6 - Fatura em HTML

--- index.js
+++ index.js
@@ -70,7 +70,28 @@ function gerarFaturaStr (fatura, pecas) {
   return faturaStr;
 }
 
+function gerarFaturaHTML(fatura, pecas) {
+  let html = `<html>\n`;
+  html += `<p> Fatura ${fatura.cliente} </p>\n`;
+  html += `<ul>\n`;
+
+  for (let apre of fatura.apresentacoes) {
+    let total = calcularTotalApresentacao(pecas, apre);
+    html += `<li> ${getPeca(pecas, apre).nome}: ${formatarMoeda(total)} (${apre.audiencia} assentos) </li>\n`;
+  }
+
+  html += `</ul>\n`;
+  html += `<p> Valor total: ${formatarMoeda(calcularTotalFatura(pecas, fatura.apresentacoes))} </p>\n`;
+  html += `<p> Créditos acumulados: ${calcularTotalCreditos(pecas, fatura.apresentacoes)} </p>\n`;
+  html += `</html>`;
+
+  return html;
+}
+
+
 const faturas = JSON.parse(readFileSync('./faturas.json'));
 const pecas = JSON.parse(readFileSync('./pecas.json'));
 const faturaStr = gerarFaturaStr(faturas, pecas);
 console.log(faturaStr);
+const faturaHTML = gerarFaturaHTML(faturas, pecas);
+console.log(faturaHTML);
